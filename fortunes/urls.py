d5fb3a866e8a4cff4ec2b91dc246a818535c9f1b
neyona <40152041+neyona@users.noreply.github.com>
2019-11-11 10:07:20 -0500

    Delete urls.py

--- fortunes/urls.py
+++
@@ -1,9 +0,0 @@
-# fortunes/urls.py
-from django.urls import path
-
-from .views import FortuneList, FortuneDetail
-
-urlpatterns = [
-    path('<int:pk>/', FortuneDetail.as_view()),
-    path('', FortuneList.as_view()),
-]
