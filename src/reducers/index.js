374e3349ecaa5cedce0b83a4a366265275675c3f
neyona <40152041+neyona@users.noreply.github.com>
2019-11-11 10:10:54 -0500

    Delete index.js

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// src/reducers/index.js
-import { combineReducers } from 'redux';
-
-import nameAuraReducer from './nameAuraReducer';
-
-export default combineReducers({
-    nameAura: nameAuraReducer,
-});
