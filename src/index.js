a0867d9058409a1cd5339168f8f70d51bf4163a1
neyona <40152041+neyona@users.noreply.github.com>
2019-11-11 10:09:55 -0500

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
